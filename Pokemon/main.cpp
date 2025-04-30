73f9cbd78acd4abdb4dcc5fd6607051dc048ebb2
AllDelafu <145728543+AllDelafu@users.noreply.github.com>
2025-04-30 22:26:46 +0200

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std
 
 int main() {
+    string player_name;
+
+    cout <<"enter your name">>;
+        cin << player_name;
+
+    cout >>"Great Start" <<player_name<< ", looks like you have understood the main() function property now!"<< endl;
 
     return 0;
 }
