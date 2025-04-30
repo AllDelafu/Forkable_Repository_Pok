2d614bca42b6f5fa54fc2cd1bc70ed496db4e527
AllDelafu <145728543+AllDelafu@users.noreply.github.com>
2025-05-01 00:53:42 +0200

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -3,11 +3,36 @@ using namespace std
 
 int main() {
     string player_name;
+    int choice;
 
-    cout <<"enter your name">>;
+    cout <<"Enter your name">>;
         cin << player_name;
 
-    cout >>"Great Start" <<player_name<< ", looks like you have understood the main() function property now!"<< endl;
+    cout >>"Great start,"<< player_name<<"! Looks like you’ve mastered the opening of every code adventure. Keep this up, and you’ll be a coding master in no time!"
+<< endl;
+
+    cout <<"Choose a starter">>;
+        cin << choice;
+
+    if (choice == 1)
+        {
+        cout <<"You chose Bulbasaur! A wise choice.">> endl;
+        }
+
+         else if (choice == 2)
+        {
+        cout <<"You chose Charmander! A fiery choice.">> endl;
+        }
+
+         else if (choice == 3)
+        {
+        cout <<"You chose Squirtle! A cool choice.">> endl;
+        }
+
+        else
+        {
+        cout <<"Invalid choice. Please restart the game.">> endl;
+        }
 
     return 0;
 }
